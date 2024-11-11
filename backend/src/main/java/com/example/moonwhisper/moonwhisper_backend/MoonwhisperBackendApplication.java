74ab1bb44c4e9b81aca7b43f1cda37133d81e48c
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-11-11 16:01:23 +0900

    first app setting

---
+++ backend/src/main/java/com/example/moonwhisper/moonwhisper_backend/MoonwhisperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.moonwhisper.moonwhisper_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonwhisperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonwhisperBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/moonwhisper/moonwhisper_backend/MoonwhisperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moonwhisper.moonwhisper_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonwhisperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
