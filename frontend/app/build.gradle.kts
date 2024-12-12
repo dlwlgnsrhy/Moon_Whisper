e68ffe26027a76bf832c5a22837ffec9f6e7566b
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-12-12 20:06:50 +0900

    return repository with local code

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -28,20 +28,29 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            isDebuggable = true
+            // 여기에 추가적인 디버그 설정을 할 수 있음
+        }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -69,4 +78,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+tasks.register("buildRelease") {
+    doLast {
+        println("Release build is being generated")
+    }
 }
